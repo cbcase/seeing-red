bd06237c7f571161584e0a09813cad1c66fbc409
None <None>
2012-05-24 19:29:52 +0000

    RED seems to be doing something

--- red-topo.py
+++ red-topo.py
@@ -6,6 +6,8 @@ from mininet.util import dumpNetConnections
 
 import sys
 import os
+from time import sleep, time
+from subprocess import Popen, PIPE
 
 class Fig4Topo(Topo):
     "Topology from figure 4 of RED paper"
@@ -45,9 +47,14 @@ class Fig4Topo(Topo):
 class Fig6Topo(Topo):
     def __init__(self):
         Topo.__init__(self)
-        src_lconfig = {'bw': 100, 'delay': '1ms', 'max_queue_size': None}
-        dst_lconfig = {'bw': 45, 'delay': '20ms', 'max_queue_size': None}
-        hconfig = {'cpu': None}
+        src_lconfig = {'bw': 100, 'delay': '1ms', 'max_queue_size': None,
+		       'enable_red': True, 'red_min': 20000, 'red_max': 25000,
+		       'red_avpkt': 1000, 'red_burst': 20, 'red_prob': 1}
+        dst_lconfig = {'bw': 45, 'delay': '20ms', 'max_queue_size': None,
+		       'enable_red': True, 'red_min': 20000, 'red_max': 25000,
+		       'red_avpkt': 1000, 'red_burst': 20, 'red_prob': 1}
+
+        hconfig = {'cpu': None, 'enable_red': True}
 
         s1 = self.add_switch('s1')
         # hosts 1..2
@@ -86,6 +93,51 @@ class Fig11Topo(Topo):
     def numSources(self):
         return 5
 
+def get_txbytes(iface):
+    f = open('/proc/net/dev', 'r')
+    lines = f.readlines()
+    for line in lines:
+        if iface in line:
+            break
+    f.close()
+    if not line:
+        raise Exception("could not find iface %s in /proc/net/dev:%s" %
+                        (iface, lines))
+    # Extract TX bytes from:
+    #Inter-|   Receive                                                |  Transmit
+    # face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+    # lo: 6175728   53444    0    0    0     0          0         0  6175728   53444    0    0    0     0       0          0
+    return float(line.split()[9])
+
+
+def get_rates(iface, nsamples=3, period=1.0,
+              wait=3.0):
+    """Returns rate in Mbps"""
+    # Returning nsamples requires one extra to start the timer.
+    nsamples += 1
+    last_time = 0
+    last_txbytes = 0
+    ret = []
+    sleep(wait)
+    while nsamples:
+        nsamples -= 1
+        txbytes = get_txbytes(iface)
+        now = time()
+        elapsed = now - last_time
+        #if last_time:
+        #    print "elapsed: %0.4f" % (now - last_time)
+        last_time = now
+        # Get rate in Mbps; correct for elapsed time.
+        rate = (txbytes - last_txbytes) * 8.0 / 1e6 / elapsed
+        if last_txbytes != 0:
+            # Wait for 1 second sample
+            ret.append(rate)
+        last_txbytes = txbytes
+        print '.',
+        sys.stdout.flush()
+        sleep(period)
+    return ret
+
 def verify_latency(net):
     sink = net.getNodeByName('sink');
     for i in range(1, net.topo.numSources() + 1):
@@ -108,8 +160,38 @@ def verify_bandwidth(net):
             net.iperf([host, other_host])
         net.iperf([host, sink])
 
+def verify_throughput(net):
+    print 'throughput at s1 s1-eth0: ' + str(get_rates('s1-eth0')) + ' Mbps'
+
+CUSTOM_IPERF_PATH = '~/iperf-patched/src/iperf'
+CONG = 'bic'
+def start_senders(net):
+    # Seconds to run iperf; keep this very high
+    recvr = net.getNodeByName('sink')
+    seconds = 3600
+    for i in range (1, 3):
+        h = net.getNodeByName('h%s' % i)
+        for j in range (0, 1): #args.nflows=1
+            h.cmd('%s -c %s -p %s -t %d -i 1 -yc -Z %s > %s/h%s.txt &' %
+                  (CUSTOM_IPERF_PATH, recvr.IP(), 5001, seconds, CONG, os.getcwd(), i))
+
+def start_receiver(net):
+    recvr = net.getNodeByName('sink')
+    recvr.cmd('%s -s -p %s > %s/iperf_server.txt &' %
+              (CUSTOM_IPERF_PATH, 5001, os.getcwd()))
+
+def start_tcpprobe():
+    "Instal tcp_pobe module and dump to file"
+    os.system("rmmod tcp_probe; modprobe tcp_probe;")
+    Popen("cat /proc/net/tcpprobe > %s/tcp_probe.txt" %
+          os.getcwd(), shell=True)
+
+def stop_tcpprobe():
+    os.system("killall -9 cat; rmmod tcp_probe &>/dev/null;")
+
+
 def main():
-    topo = Fig11Topo()
+    topo = Fig6Topo()
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
     net.start()
     dumpNetConnections(net)
@@ -117,6 +199,13 @@ def main():
 
     verify_latency(net)
     verify_bandwidth(net)
+    start_receiver(net)
+    start_tcpprobe()
+    start_senders(net)
+    verify_throughput(net)
+    #Popen("killall -9 top bwm-ng tcpdump cat", shell=True).wait()
+    #stop_tcpprobe()
+    #os.system('killall -9 ' + CUSTOM_IPERF_PATH)
     #os.system("tc qdisc change dev s1-eth0 root red limit 1000000 min 10000 max 100000 avpkt 1000 burst 15 probability 0.01")
     #os.system("tc qdisc show")
 
