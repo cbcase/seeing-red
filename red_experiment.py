984af3ba776679055e2ab850a18cdb99b88b9a45
None <None>
2012-05-25 02:55:07 +0000

    Added some files, moved some files around

--- red_experiment.py
+++ red_experiment.py
@@ -8,90 +8,10 @@ import sys
 import os
 from time import sleep, time
 from subprocess import Popen, PIPE
+import termcolor as T
+from argparse import ArgumentParser
 
-class Fig4Topo(Topo):
-    "Topology from figure 4 of RED paper"
-
-    def __init__(self):
-        Topo.__init__(self);
-
-        l1config = {'bw': 100, 'delay': '1ms', 'max_queue_size': None }
-        l2config = {'bw': 100, 'delay': '4ms', 'max_queue_size': None }
-        l3config = {'bw': 100, 'delay': '8ms', 'max_queue_size': None }
-        l4config = {'bw': 100, 'delay': '5ms', 'max_queue_size': None }
-        l6config = {'bw': 45, 'delay': '2ms', 'max_queue_size': None }
-
-        hconfig = {'cpu': None}
-
-        s1 = self.add_switch('s1')
-        h1 = self.add_host('h1', **hconfig);
-        h2 = self.add_host('h2', **hconfig);
-        h3 = self.add_host('h3', **hconfig);
-        h4 = self.add_host('h4', **hconfig);
-        sink = self.add_host('sink', **hconfig);
-
-        self.add_link(h1, s1,
-                      port1=0, port2=1, **l1config);
-        self.add_link(h2, s1,
-                      port1=0, port2=2, **l2config);
-        self.add_link(h3, s1,
-                      port1=0, port2=3, **l3config);
-        self.add_link(h4, s1,
-                      port1=0, port2=4, **l4config);
-        self.add_link(s1, sink,
-                       port1=0, port2=0, **l6config);
-
-    def numSources(self):
-        return 4
-
-class Fig6Topo(Topo):
-    def __init__(self):
-        Topo.__init__(self)
-        src_lconfig = {'bw': 100, 'delay': '1ms', 'max_queue_size': None,
-		       'enable_red': True, 'red_min': 20000, 'red_max': 25000,
-		       'red_avpkt': 1000, 'red_burst': 20, 'red_prob': 1}
-        dst_lconfig = {'bw': 45, 'delay': '20ms', 'max_queue_size': None,
-		       'enable_red': True, 'red_min': 20000, 'red_max': 25000,
-		       'red_avpkt': 1000, 'red_burst': 20, 'red_prob': 1}
-
-        hconfig = {'cpu': None, 'enable_red': True}
-
-        s1 = self.add_switch('s1')
-        # hosts 1..2
-        for i in range(1, 3):
-            host = self.add_host('h%d' % i, **hconfig)
-            self.add_link(host, s1, port1=0, port2=i, **src_lconfig)
-
-        sink = self.add_host('sink', **hconfig)
-        self.add_link(s1, sink, port1=0, port2=0, **dst_lconfig)
-
-    def numSources(self):
-        return 2
-
-class Fig11Topo(Topo):
-    def __init__(self):
-        Topo.__init__(self)
-        l1config = {'bw': 100, 'delay': '1ms', 'max_queue_size': None }
-        l2config = {'bw': 45, 'delay': '16ms', 'max_queue_size': None }
-        l3config = {'bw': 45, 'delay': '2ms', 'max_queue_size': None }
-
-        hconfig = {'cpu': None }
-        s1 = self.add_switch('s1')
-        # hosts 1..4
-        for i in range(1, 5):
-            host = self.add_host('h%d' % i, **hconfig)
-            self.add_link(host, s1, port1=0,
-                          port2=i, **l1config)
-        # host 5
-        h5 = self.add_host('h5', **hconfig)
-        self.add_link(h5, s1, port1=0, port2=5, **l2config)
-
-        # sink
-        sink = self.add_host('sink', **hconfig)
-        self.add_link(s1, sink, port1=0, port2=0, **l3config)
-
-    def numSources(self):
-        return 5
+from red_topo import *
 
 def get_txbytes(iface):
     f = open('/proc/net/dev', 'r')
@@ -189,8 +109,7 @@ def start_tcpprobe():
 def stop_tcpprobe():
     os.system("killall -9 cat; rmmod tcp_probe &>/dev/null;")
 
-
-def main():
+def run_debug():
     topo = Fig6Topo()
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
     net.start()
@@ -203,13 +122,60 @@ def main():
     start_tcpprobe()
     start_senders(net)
     verify_throughput(net)
-    #Popen("killall -9 top bwm-ng tcpdump cat", shell=True).wait()
-    #stop_tcpprobe()
-    #os.system('killall -9 ' + CUSTOM_IPERF_PATH)
-    #os.system("tc qdisc change dev s1-eth0 root red limit 1000000 min 10000 max 100000 avpkt 1000 burst 15 probability 0.01")
-    #os.system("tc qdisc show")
-
     net.stop()
+
+def run_experiment_one():
+    #set up Fig6Topo
+    topo = Fig6Topo()
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+    dumpNetConnections(net)
+    net.pingAll()
+
+    #open up TCP connections with max windows of 240 packets (roughly bw-delay product)
+    #    NOTE: packets are 1000 bytes
+    #    QUESTION: do I need to start_server/recvr?
+    #get maximum possible throughput on link
+    #run five 5-sec simulations for each of 11 sets of params for each DT and RED
+    #    drop tail: 15 to 140 packets (15, 30, 45, 60, 75, 90, 100, 110, 120, 130, 140)
+    #    RED: max buffer size of  100 packets,
+    #         min_th ranging from 3 to 50 packets
+    #         max_th := 3*min_th
+    #         w_q := 0.002
+    #         max_p := 1/50
+    #
+    #get average queue size over this interval (HOW?????)
+    #compute throughput (can just say total_bytes/5sec)
+    pass
+
+def run_experiment_two():
+    pass
+
+def main():
+    parser = ArgumentParser(description="RED experiments")
+    parser.add_argument('--exp1',
+                        action='store_true',
+                        help='Run Experiment 1')
+    parser.add_argument('--exp2',
+                        action='store_true',
+                        help='Run Experiment 2')
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='Run test')
+
+    args = parser.parse_args()
+
+
+    if not args.exp1 and not args.exp2 and not args.debug:
+        print T.colored('You forgot to specify an experiment to run.\n' +
+                        'Usage: red_experiment.py [-h] [--exp1] [--exp2]',
+			'green')
+    if args.exp1:
+        run_experiment_one()
+    if args.exp2:
+        run_experiment_two()
+    if args.debug:
+        run_debug()
 
 if __name__ =='__main__':
     main()
---
+++ red_topo.py
@@ -0,0 +1,95 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNetConnections
+
+import sys
+import os
+from time import sleep, time
+from subprocess import Popen, PIPE
+
+class Fig4Topo(Topo):
+    "Topology from figure 4 of RED paper"
+
+    def __init__(self):
+        Topo.__init__(self);
+
+        l1config = {'bw': 100, 'delay': '1ms', 'max_queue_size': None }
+        l2config = {'bw': 100, 'delay': '4ms', 'max_queue_size': None }
+        l3config = {'bw': 100, 'delay': '8ms', 'max_queue_size': None }
+        l4config = {'bw': 100, 'delay': '5ms', 'max_queue_size': None }
+        l6config = {'bw': 45, 'delay': '2ms', 'max_queue_size': None }
+
+        hconfig = {'cpu': None}
+
+        s1 = self.add_switch('s1')
+        h1 = self.add_host('h1', **hconfig);
+        h2 = self.add_host('h2', **hconfig);
+        h3 = self.add_host('h3', **hconfig);
+        h4 = self.add_host('h4', **hconfig);
+        sink = self.add_host('sink', **hconfig);
+
+        self.add_link(h1, s1,
+                      port1=0, port2=1, **l1config);
+        self.add_link(h2, s1,
+                      port1=0, port2=2, **l2config);
+        self.add_link(h3, s1,
+                      port1=0, port2=3, **l3config);
+        self.add_link(h4, s1,
+                      port1=0, port2=4, **l4config);
+        self.add_link(s1, sink,
+                       port1=0, port2=0, **l6config);
+
+    def numSources(self):
+        return 4
+
+class Fig6Topo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        src_lconfig = {'bw': 100, 'delay': '1ms', 'max_queue_size': None,
+		       'enable_red': True, 'red_min': 20000, 'red_max': 25000,
+		       'red_avpkt': 1000, 'red_burst': 20, 'red_prob': 1.0}
+        dst_lconfig = {'bw': 45, 'delay': '20ms', 'max_queue_size': None,
+		       'enable_red': True, 'red_min': 20000, 'red_max': 25000,
+		       'red_avpkt': 1000, 'red_burst': 20, 'red_prob': 1.0}
+
+        hconfig = {'cpu': None, 'enable_red': True}
+
+        s1 = self.add_switch('s1')
+        # hosts 1..2
+        for i in range(1, 3):
+            host = self.add_host('h%d' % i, **hconfig)
+            self.add_link(host, s1, port1=0, port2=i, **src_lconfig)
+
+        sink = self.add_host('sink', **hconfig)
+        self.add_link(s1, sink, port1=0, port2=0, **dst_lconfig)
+
+    def numSources(self):
+        return 2
+
+class Fig11Topo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        l1config = {'bw': 100, 'delay': '1ms', 'max_queue_size': None }
+        l2config = {'bw': 45, 'delay': '16ms', 'max_queue_size': None }
+        l3config = {'bw': 45, 'delay': '2ms', 'max_queue_size': None }
+
+        hconfig = {'cpu': None }
+        s1 = self.add_switch('s1')
+        # hosts 1..4
+        for i in range(1, 5):
+            host = self.add_host('h%d' % i, **hconfig)
+            self.add_link(host, s1, port1=0,
+                          port2=i, **l1config)
+        # host 5
+        h5 = self.add_host('h5', **hconfig)
+        self.add_link(h5, s1, port1=0, port2=5, **l2config)
+
+        # sink
+        sink = self.add_host('sink', **hconfig)
+        self.add_link(s1, sink, port1=0, port2=0, **l3config)
+
+    def numSources(self):
+        return 5
+
